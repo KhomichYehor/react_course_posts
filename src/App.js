d6fe690e664cf4ce9aadac13222ca2f18de9e835
Yehor <eee.houmich@gmail.com>
2024-03-21 21:15:13 +0200

    Create PostList component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles/App.css';
 
-import PostItem from "./components/PostItem/PostItem";
+import PostsList from "./components/PostList/PostsList";
 
 export const App = () => {
+    const [posts, setPosts] = useState([
+        {id: 1, title:'JavaScript: Basic', body:'Description'},
+        {id: 2, title:'JavaScript: Advanced', body:'Description'},
+        {id: 3, title:'JavaScript: Promise', body:'Description'},
+        {id: 4, title:'JavaScript: Classes', body:'Description'},
+        {id: 5, title:'JavaScript: Prototypes', body:'Description'},
+    ])
+
     return (
         <div className="App">
-            <PostItem />
+            <PostsList posts={posts} title='JavaScript Posts'/>
         </div>
     )
 }
